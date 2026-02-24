ec333e3f8b104b9037f4029ecb3dd6a9298b7d8b
sameer512100 <sayyedsameer2106@gmail.com>
2026-02-24 23:52:33 +0530

    solved 24/2/26 potd

---
+++ BitManipulation/1022.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int solve(TreeNode *root, int val)
+    {
+        if (!root)
+        {
+            return 0;
+        }
+        val = (2 * val) + root->val;
+        if (!root->left && !root->right)
+        {
+            return val;
+        }
+        return solve(root->left, val) + solve(root->right, val);
+    }
+    int sumRootToLeaf(TreeNode *root)
+    {
+        return solve(root, 0);
+    }
+};
