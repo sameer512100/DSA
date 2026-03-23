86cd7b27822279175f4353205c86de8b90cd462e
sameer512100 <sayyedsameer2106@gmail.com>
2026-03-24 00:46:48 +0530

    LL is not fun

---
+++ LinkedLists/SegOdd&Eve.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    ListNode *oddEvenList(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL)
+        {
+            return head;
+        }
+
+        ListNode *odd = head;
+        ListNode *even = head->next;
+        ListNode *evenHead = even;
+
+        while (even != NULL && even->next != NULL)
+        {
+            odd->next = odd->next->next;
+            even->next = even->next->next;
+
+            odd = odd->next;
+            even = even->next;
+        }
+
+        odd->next = evenHead;
+        return head;
+    }
+};
